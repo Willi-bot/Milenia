5b01b3bb969d62264647193e4c97034732590384
Willi <willi_8_7@gmx.de>
2022-09-13 11:47:30 +0200

    Rename scenes for consistent naming convention

--- Milenia/Assets/Scripts/SceneLoadActions.cs
+++ Milenia/Assets/Scripts/SceneLoadActions.cs
@@ -4,34 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoadActions : MonoBehaviour
 {
-    [SerializeField] private Transform player; //drag player reference onto here
+    [SerializeField] private Transform player;
     private Vector3 targetPosition;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     private void OnEnable()
     {
-        SceneManager.sceneLoaded += SceneLoaded; //You add your method to the delegate
+        SceneManager.sceneLoaded += SceneLoaded;
     }
      
     private void OnDisable()
     {
         SceneManager.sceneLoaded -= SceneLoaded;
     }
-     
-    //After adding this method to the delegate, this method will be called every time
-    //that a new scene is loaded. You can then compare the scene loaded to your desired
-    //scenes and do actions according to the scene loaded.
+    
+    
     private void SceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (gameObject.scene.name == "PlayerBaseScene" && Level.PreviousLevel == "TowerMap")
+        if (gameObject.scene.name == "PlayerBase" && Level.PreviousLevel == "Tower")
         {
             player.position = new Vector3(0, 0, 0);
         }
