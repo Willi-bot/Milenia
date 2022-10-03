cb01a32bb18bbbdc898b13a5c3b9ea82215369af
hubanton <anton.huber98@yahoo.com>
2022-10-03 13:41:03 +0200

    Add first basic Animation for Player

--- Milenia/Assets/Scripts/Player.cs
+++ Milenia/Assets/Scripts/Player.cs
@@ -7,25 +7,39 @@ public class Player : MonoBehaviour
     private BoxCollider2D _boxCollider;
     private Vector2 _colliderOffset;
 
+    private Animator _animator;
+    
     private Vector3 _moveDelta;
 
     private RaycastHit2D _hit;
-    
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
     // Start is called before the first frame update
     void Start()
     {
         _boxCollider = GetComponent<BoxCollider2D>();
         _colliderOffset = _boxCollider.offset;
+        _animator = GetComponent<Animator>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        _moveDelta = new Vector3(x, y, 0);
+        
+        _animator.SetFloat(Horizontal, x);
+        _animator.SetFloat(Vertical, y);
 
+        _moveDelta = new Vector3(x, y, 0);
+        
+        
+        _animator.SetFloat(Speed, _moveDelta.sqrMagnitude);
+        
         Vector3 transPos = transform.position;
         Vector3 hitBoxPosition = new Vector3(transPos.x + _colliderOffset.x, 
             transPos.y + _colliderOffset.y, 0);
@@ -44,5 +58,7 @@ public class Player : MonoBehaviour
         {
             transform.Translate(_moveDelta.x * (Time.deltaTime * 3),0, 0);
         }
+        
+        
     }
 }
