93de2b9cc0541f50fa800bcdc064ca4f42514ea4
hubanton <anton.huber98@yahoo.com>
2022-10-06 12:13:04 +0200

    Improve Animation, add Border to pit

--- Milenia/Assets/Scripts/Player.cs
+++ Milenia/Assets/Scripts/Player.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
+    public float playerSpeed = 4.5f;
+    
     private BoxCollider2D _boxCollider;
     private Vector2 _colliderOffset;
 
@@ -45,18 +48,18 @@ public class Player : MonoBehaviour
             transPos.y + _colliderOffset.y, 0);
         
         _hit = Physics2D.BoxCast(hitBoxPosition, _boxCollider.size, 0, 
-            new Vector2(0, _moveDelta.y), Mathf.Abs(_moveDelta.y * Time.deltaTime * 3), 
+            new Vector2(0, _moveDelta.y), Mathf.Abs(_moveDelta.y * Time.deltaTime * playerSpeed), 
             LayerMask.GetMask("Default", "MapBounds", "TriggerBox"));
         if (_hit.collider is null)
         {
-            transform.Translate(0, _moveDelta.y * (Time.deltaTime * 3), 0);
+            transform.Translate(0, _moveDelta.y * (Time.deltaTime * playerSpeed), 0);
         }
         _hit = Physics2D.BoxCast(hitBoxPosition, _boxCollider.size, 0, 
-            new Vector2(_moveDelta.x, 0), Mathf.Abs(_moveDelta.x * Time.deltaTime * 3), 
+            new Vector2(_moveDelta.x, 0), Mathf.Abs(_moveDelta.x * Time.deltaTime * playerSpeed), 
             LayerMask.GetMask("Default", "MapBounds"));
         if (_hit.collider is null)
         {
-            transform.Translate(_moveDelta.x * (Time.deltaTime * 3),0, 0);
+            transform.Translate(_moveDelta.x * (Time.deltaTime * playerSpeed),0, 0);
         }
         
         
